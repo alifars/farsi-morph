52017d85651e557c19a6a9a7554a1729b2b5de7c
Wes <hermes@ubuntu.(none)>
2013-04-18 02:19:39 -0400

    fixed some mistakes, development underway

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Main repository for Farsi lemmatizer project.
+Farsi-Morph
+Weston Feely
+4/18/13
+
+Main repository for Farsi morphological analyzer project.
+
+To Do List:
+- (Done) Extract (token,lemma,POS) triples from Farsi treebank
+- (Done) Write a script to randomly divide lemma triples into three corpora A, B, C
+- (Done) Write a script to print all lemmas for a given POS from a given corpus to stdout
+- (Done) Set up farsi.foma and lexicon files
+- (Done) Fix print_lemmas.py to strip whitespace and ZWNJ from sides of lemmas
+- (Done) Put lemmas from A corpus into lexicon files
+- (Done) Write basic morphology into lexicon for each open-class POS category
+- (In Progress) Modify lexical morphology to allow repeated application of suffixes
+- Clean up obvious errors in lemmas in corpora (missing lemma '_', clearly erroneous lemmas)
+- Make an evaluation script, to get precision-recall for a given corpus.
+- Evaluate on A corpus.
+- Put lemmas from B corpus into lexicon files
+	- Modify print_lemmas.py to overwrite existing lemmas below hand-added entries
+	- Print lemmas from A and B corpus, redirect and append to lexicon files
+- Evaluate on B corpus.
+- Put lemmas from C corpus into lexicon files
+- Evaluate on C corpus.
+- (In Progress) Update report.
+	- (In Progress) Explain POS categories, marking open-class and closed-class
+	- (In Progress) Explain Farsi text issues with ZWNJ and solutions found
+	- (Done) Explain Farsi verb tokenization issue from treebank
+	- Include precision-recall results from each run
--- print_lemmas.py
+++ print_lemmas.py
@@ -1,24 +1,48 @@
 #!/usr/bin/python
 #print_lemmas.py
 #Weston Feely
-#4/8/13
-import sys
+#4/18/13
+import sys, re
 
 def main(args):
 	#Check for required args
 	if len(args) < 3:
 		print 'Usage: python print_lemmas.py corpus part-of-speech >> ../src/lexica/part-of-speech.lexc'
 		return 1
+	#Read in args
 	corpus = open(args[1]).readlines()
 	check_pos = args[2].lower()
+	#Add all lemmas with specified POS tag to lemmas set, token+lemma pairs to pairs set
 	lemmas = set()
-	#Add all lemmas with specified POS tag to lemmas set
+	pairs = set()
 	for line in corpus:
-		lis = line.split('\t')
-		lemma = lis[1]
+		#Strip whitespace off line edges and split on tab
+		lis = line.strip().split('\t')
+		#Strip token edges of whitespace and ZWNJ
+		token = lis[0].strip().decode('utf-8').strip(u'\u200c').encode('utf-8')
+		#Substitute ZWNJ for space internally in token
+		token = re.sub(' ','\xe2\x80\x8c',token)
+		#Substitute "~" for ZWNJ internally in token	
+		token = re.sub('\xe2\x80\x8c','~',token)
+		#Strip lemmas edges of whitespace and ZWNJ
+		lemma = lis[1].strip().decode('utf-8').strip(u'\u200c').encode('utf-8')
+		#Substitute ZWNJ for space internally in token
+		lemma = re.sub(' ','\xe2\x80\x8c',lemma)
+		#Strip pos tag edges of whitespace and lowercase it
 		pos = lis[2].strip().lower()
+		#If this is the POS we're looking for, add the lemma to our list
 		if check_pos == pos:
-			lemmas.add(lemma)
+			if pos != 'v':
+				lemmas.add(lemma)
+			else:
+				#For verb lemmas, add every verb stem to lemma set 
+				for l in lemma.split('#'):
+					lemmas.add(l)
+			if token != lemma:
+				pairs.add((token,lemma))
+	#Write pairs to stdout
+	#for pair in pairs:
+	#	print pair[0]+'\t'+pair[1]
 	#Write lemmas to stdout
 	for lemma in lemmas:
 		#Print "Lemma (tab) PosInf;"
