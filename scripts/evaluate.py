ecb4786ca6ff19b5dbbae8e4592eaa60d36b71d9
Wes <hermes@ubuntu.(none)>
2013-04-25 17:13:11 -0400

    fixed bug in evaluation script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -10,15 +10,22 @@ def main(args):
 		print "Usage: python evaluate.py corpus.lemma corpus.results"
 		return 1
 	#Read in gold standard lemmas
-	gold = [x.strip() for x in open(args[1]).readlines()]
+	gold = []
+	for lemma in open(args[1]):
+		lemma = lemma.strip()
+		if '#' in list(lemma):
+			gold.append(tuple([x.strip() for x in lemma.split('#')]))
+		else:
+			gold.append(lemma)
 	#Read in lemma hypotheses from lemmatizer
 	hyp_raw = open(args[2]).readlines()
-	hyp = [] # list of lists, hypotheses for each token
-	buff = []
+	hyp = [] # list of sets, hypotheses for each token
+	buff = set() # buffer set for current hypothesis
 	for line in hyp_raw:
 		if line.isspace():
 			#New lemma, append hypotheses for last lemma to hyp list
 			hyp.append(buff)
+			buff = set() # restart buffer
 		else:
 			#New hypothesis for current lemma
 			line = line.strip() # strip whitespace
@@ -27,15 +34,35 @@ def main(args):
 			#print "After "+line
 			#Append cleaned-up hyp to current buffer
 			if (line != '') and (not line.isspace()):
-				buff.append(line)
+				buff.add(line)
 	assert len(gold) == len(hyp)
 	#Get an accuracy score for this corpus	
 	acc = 0
 	i = 0
 	for lemma in gold:
-		if lemma in hyp[i]:
-			acc += 1
-		i+=1
+		'''
+		#Set up print statement for debugging
+		lexp = ''
+		if type(lemma) is tuple:
+			lexp = ' '.join(lemma)
+		else:
+			lexp = lemma
+		rexp = ' '.join(hyp[i])
+		print "Lemma: ["+lexp+"] | Hyp: ["+rexp+"]"
+		'''		
+		#Check if lemma is a tuple (for verb lemma)
+		if type(lemma) is tuple:
+			#Loop through verb lemmas in lemma tuple
+			for verb_lemma in lemma:
+				#If any hypothesis lemma is the same as this gold verb lemma
+				if verb_lemma in hyp[i]:
+					acc += 1 # count this as an accurate hypothesis
+					break # don't give credit for multiple accurate verb lemma hypotheses
+		else:
+			#If any hypothesis lemma is the same as the gold lemma
+			if lemma in hyp[i]:
+				acc += 1 # count this as an accurate hypothesis
+		i+=1 # move to next hypothesis
 	acc = float(acc)/i
 	#Write accuracy to stdout
 	print acc
